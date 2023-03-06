0942b2481982542ccc9b106bbce3a746b7d5f394
МалевАВ <@211-01>
2023-03-06 09:28:57 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WSUniversalLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetProfityForAgeTest()
+        {
+            int rez = 115;
+
+            Calculation calculation = new Calculation();
+
+            int fak = calculation.GetProfityForAgent(1, 23, 10);
+
+            Assert.AreEqual(rez, fak);
+        }
+    }
+}
---
+++ WSUniversalLib/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int GetProfityForAgent(int agentType, float age, float exprience)
+        {
+            float agent = age * exprience;
+
+            int res = 0;
+
+            float promres = 0;
+
+            if (agent < 0 || res < 0)
+            {
+                return -1;
+            }
+
+            switch (agentType)
+            {
+                case 1:
+                {
+                    switch (agentType)
+                    {
+                        case 1:
+                        {
+                            if (exprience >= 10)
+                            { 
+                                promres = (float)(agent * 0.5);           
+
+                                res = (int)Math.Ceiling(promres);
+
+                            }                        
+
+                            return res;
+                        }
+
+                            case 2:
+                                {
+                                    if (exprience >= 20)
+
+                                    {
+
+                                        promres = (float)(agent * 0.7);
+
+                                        res = (int)Math.Ceiling(promres);
+
+                                    }
+
+                                    return res;
+                                }
+
+                            case 3:
+                                {
+                                    if (exprience >= 40)
+
+                                    {
+                                        promres = (float)(agent * 0.9);
+
+                                        res = (int)Math.Ceiling(promres);
+
+                                    }
+
+                                    return res;
+                                }
+
+                            default:
+                                {
+                                    return -1;
+                                }
+                    }
+                }
+
+                case 2:
+                    {
+                        switch (agentType)
+                        {
+                            case 1:
+                                {
+                                    promres = (float)(agent * 0.1);
+
+                                    res = (int)Math.Ceiling(promres);
+
+                                    return res;
+                                }
+
+                            case 2:
+                                {
+                                    promres = (float)(agent * 0.17);
+
+                                    res = (int)Math.Ceiling(promres);
+
+                                    return res;
+                                }
+
+                            case 3:
+                                {
+                                    promres = (float)(agent * 0.26);
+
+                                    res = (int)Math.Ceiling(promres);
+
+                                    return res;
+                                }
+
+                            default:
+                                {
+                                    return -1;
+                                }
+                        }
+                    }
+
+                default:
+                    {
+                        return -1;
+                    }
+
+            }
+        }
+    }
+}
